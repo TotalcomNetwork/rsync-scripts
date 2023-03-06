f4febb85a926a21cc2ee5593c42014aba5100766
DavideTriso <davidetrisolini@yahoo.it>
2023-03-06 15:26:32 +0100

    Hello world

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# rsyn-script
-Sync a directory on a remote host
+# Rsyn Script
+
+Sync a directory on a remote host: the `rsync-dir.sh` script copies all new/updated files found in the given directory (on the remote host) to the destination directory on the local machine.
+
+> IMPORTANT: the script requires `sshpass`. Make sure this module is installed on the local machine which executes the script, or install it with `sudo apt install sshpass`.
+
+## Usage
+
+The path to a `.env` file must be provided to the command:
+
+```bash
+./rsync-dir.sh "/path/to/my/.env"
+```
+
+The `./env` file contains all required variables to run the script. 
+See [.env.reference](.env.reference) for a list of mandatory and available variables.
+
---
+++ rsync-dir.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+
+if  [ -n "$1" ] 
+then
+    source $1
+else
+    echo "Error: missing .env file path"
+	exit 1;
+fi
+
+if  [ -z "$ssh_port" ] 
+then
+    ssh_port=22
+fi
+
+if  [ -z "$destination_owner_user" ] 
+then
+    destination_owner_user=www-data
+fi
+
+if  [ -z "$destination_owner_group" ] 
+then
+    destination_owner_group=www-data
+fi
+
+if [ -z "$remote_server_ip" ] || [ -z "$ssh_user" ] || [ -z "$ssh_password" ] || [ -z "$source" ] || [ -z "$destination" ]
+then
+	echo "Error: 5 mandatory env variables expected: remote_server_ip, ssh_user, ssh_password, source, destination"
+	exit 1;
+fi
+
+sshpass -p "$ssh_password" rsync  -e "ssh -p $ssh_port"\
+        --recursive \
+        --mkpath \
+        --update \
+        --compress \
+        --compress-level 9 \
+        --partial \
+        --progress \
+        --verbose \
+        "$ssh_user@$remote_server_ip:$source" $destination
+
+chown -R $destination_owner_user:$destination_owner_group $destination
